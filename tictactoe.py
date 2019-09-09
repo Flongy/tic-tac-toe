782a2905b39b67f1a23af9a2c1dadd6f68bd1971
Flongy <craftbymax@gmail.com>
2019-09-09 12:02:54 +0300

    Draw condition and messages

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+class Message:
+    WINMESSAGE = (0, "Вы победили!")            # Сообщение при победе
+    LOSEMESSAGE = (1, "Вы проиграли!")          # ...при поражении
+    DRAWMESSAGE = (2, "Ничья")                  # ...при ничьей
+    STEPMESSAGE = (3, "Вы сделали свой ход")     # ...на каждом шаге
+    COLLISIONMESSAGE = (4, "Вы поставили в занятую клетку")                 # ...при установке в занятую клетку
+    OTHERCOLLIDEDMESSAGE = (5, "Кто-то поставил фишку в занятую клетку")    # ...если кто-то другой поставил фишку в занятую клетку
+
+
 class Map:
     def __init__(self, size: tuple, numerate: bool = False):
         self.size = size
@@ -63,83 +72,106 @@ class Map:
             y = number % self.size[1]
         elif position is not None:
             x, y = position
-        print(x, y)
+
         start = 0
         while x >= 0 and self.size[1] > y >= 0:
             # Найти границу поля, находящуюся на одной диагонали с полученной клеткой
             start = (x, y)
             x -= 1
             y += step
-        print(start, step)
+
+        # Какая дигональ нужна:
         if counter:
+            # Справа налево "/"
             return self.__map[self.convert(start):self.convert(start) + ((start[1] * self.size[1]) or 1):self.size[1] - 1]
         else:
+            # Слева направо "\"
             return self.__map[self.convert(start)::self.size[1] + 1]
 
     def is_filled(self):
-        return len(set(self.__map)) == 1
+        return -1 not in self.__map
 
 
 class Game:
     WINREWARD = 10              # Награда при победе
     LOSEREWARD = -10            # ...при поражении
+    DRAWREWARD = 0              # ...при ничьей
     STEPREWARD = -1             # ...на каждом шаге
     COLLISIONREWARD = -100      # ...при установке в занятую клетку
+    OTHERCOLLIDEDREWARD = 0     # ...при установке кем-то другим в занятую клетку
 
     def __init__(self, players=2, size=(3, 3), in_a_row=3):
-        self.players = players
-        self.size = size
-        self.in_a_row = in_a_row
+        self.players = players      # Количество игроков
+        self.size = size            # Размер поля (h, w)
+        self.in_a_row = in_a_row    # Количество фишек вряд для победы
 
-        self.game_map = Map(self.size)
-        self.current_player = 0
-        self.done = False
-        self.winner = -1
-        # 1 + len (size * size) + len * (len - 1) + len * (len - 1) * (len - 2)
+        self.game_map = Map(self.size)  # Игровое поле
+        self.current_player = 0         # Какой игрок ходит
+        self.done = False               # Была ли закончена партия
+        self.winner = -1                # Номер победившего игрока
+                                        # (-2 - кто-то поставил в занятую клетку, -1 - ничья или партия продолжается)
 
     def reset(self):
+        """ Сброс игры """
         self.game_map = Map(self.size)
         self.current_player = 0
         self.done = False
         self.winner = -1
 
     def action(self, action):
+        """ Совершить действие (номер действия - позиция на поле) """
         if self.done:
             # Если партия уже закончена
             if self.winner == -1:
+                # Ничья
+                reward = self.DRAWREWARD
+                msg = Message.DRAWMESSAGE
+            elif self.winner == -2:
                 # Кто-то поставил фишку в занятую клетку
-                reward = self.STEPREWARD
+                reward = self.OTHERCOLLIDEDREWARD
+                msg = Message.OTHERCOLLIDEDMESSAGE
             elif self.current_player != self.winner:
                 # Проигравший
                 reward = self.LOSEREWARD
+                msg = Message.LOSEMESSAGE
             elif self.current_player == self.winner:
                 # Победитель
                 reward = self.WINREWARD
+                msg = Message.WINMESSAGE
 
             self.next_player()
-            return self.game_map, reward, True, None
+            return self.game_map, reward, True, msg
 
         if not self.game_map.set(self.current_player, action):
             # Если игрок пытается поставить фишку на занятую клетку
             self.done = True
+            self.winner = -2
             self.next_player()
-            return self.game_map, self.COLLISIONREWARD, True, None
+            return self.game_map, self.COLLISIONREWARD, True, Message.COLLISIONMESSAGE
 
         if self.find_in_a_row(action):
             # Найден победитель
             self.done = True
             self.winner = self.current_player
             self.next_player()
-            return self.game_map, self.WINREWARD, True, None
+            return self.game_map, self.WINREWARD, True, Message.WINMESSAGE
 
+        if self.game_map.is_filled():
+            # Поле было заполнено
+            self.done = True
+            self.winner = -1
+            self.next_player()
+            return self.game_map, self.DRAWREWARD, True, Message.DRAWMESSAGE
+
+        # Партия продолжается
         self.next_player()
-        return self.game_map, self.STEPREWARD, False, None
+        return self.game_map, self.STEPREWARD, False, Message.STEPMESSAGE
 
     def find_in_a_row(self, last_action):
-        def same(lst: list, in_a_row=3):
+        """ Определение победителя: стоят ли фишки в ряд (в количестве in_a_row) """
+        def same(lst: list, in_a_row):
             if len(lst) >= in_a_row:
                 for i in range(len(lst) - in_a_row + 1):
-                    print(set(lst[i:i + in_a_row]), lst[i:i + in_a_row])
                     if len(set(lst[i:i + in_a_row])) == 1 and lst[i] != -1:
                         return True
             return False
@@ -171,12 +203,16 @@ class Game:
 
 
 if __name__ == "__main__":
-    env = Game(2, (3, 4))
+    env = Game(2, (3, 3))
     print(env.game_map)
     print(env.action(0))
-    print(env.action(5))
     print(env.action(1))
-    print(env.action(6))
     print(env.action(2))
+    print(env.action(3))
+    print(env.action(4))
+    print(env.action(8))
+    print(env.action(7))
+    print(env.action(6))
+    print(env.action(5))
     print(env.action(4))
     print(env.game_map)
