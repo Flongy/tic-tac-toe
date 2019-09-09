1471632e44d66654fb057c1192ec8ab953168879
Flongy <craftbymax@gmail.com>
2019-09-09 23:04:33 +0300

    QLearning agent v3 and user input agent

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from tictactoe import Game, Message
-from agents import QAgent, RAgent, UAgent
+from agents import QAgent, RAgent, UIAgent
 
 
 def train(players, size, in_a_row, agents, episodes):
@@ -15,6 +15,7 @@ def train(players, size, in_a_row, agents, episodes):
         msgs = [None] * players
         state = env.reset().state
         while not min(done):
+            # Пройти по всем пользователям, пока для всех не закончится партия
             for j, ag in enumerate(agents):
                 if not done[j]:
                     action = ag.action(state)
@@ -29,6 +30,7 @@ def train(players, size, in_a_row, agents, episodes):
             if Message.DRAWMESSAGE in msgs:
                 draws += 1
             else:
+                # Посчитать победы
                 for j, m in enumerate(msgs):
                     if m == Message.WINMESSAGE:
                         wins[j] += 1
@@ -36,12 +38,14 @@ def train(players, size, in_a_row, agents, episodes):
                         loses[j] += 1
 
             if episode % 10_000 == 0:
+                # Отобразить статистику
                 print(f"Игра №{episode}")
                 for j in range(players):
                     print(f"\tИгрок {j}. Побед: {wins[j]}. Поражений: {loses[j]}")
                 print(f"\tНичьих: {draws}")
 
             if episode % 20_000 == 0 and (Message.WINMESSAGE in msgs or Message.DRAWMESSAGE in msgs):
+                # Отобразить поле в конце партии
                 if Message.DRAWMESSAGE in msgs:
                     print(Message.DRAWMESSAGE[1])
                 else:
@@ -49,6 +53,7 @@ def train(players, size, in_a_row, agents, episodes):
                 print(env.game_map)
 
             if episode % 200_000 == 0 and episode:
+                # Сохранить Q-таблицы
                 for j, ag in enumerate(agents):
                     # ag.save(f"dumps/Player{j}-{players}-{size[0]}x{size[1]}-{in_a_row}-{episode}eps-{int(time.time())}.pickle")
                     ag.save(f"dumps/Player{j}-{players}-{size[0]}x{size[1]}-{in_a_row}-last.pickle")
@@ -64,12 +69,12 @@ if __name__ == "__main__":
 
     # Компьютеры, участвующие в игре
     # agents = [QAgent(size) for _ in range(players)]
-    # agents = [RAgent(size), QAgent(size, episodes=episodes)]
+    agents = [RAgent(size), QAgent(size, episodes=episodes)]
     # agents = [QAgent.load("dumps/Player0-2-4x4-4-last.pickle", size, False),
     #           QAgent.load("dumps/Player1-2-4x4-4-last.pickle", size, False)]
     # agents = [RAgent(size), QAgent.load("dumps/Player1-2-4x4-4-last.pickle", size, True, episodes)]
     # agents = [QAgent.load("dumps/Player0-2-4x4-4-last.pickle", size, False), RAgent(size)]
-    agents = [RAgent(size), UAgent(size)]
+    # agents = [RAgent(size), UIAgent(size)]
 
     # Запуск цикла тренировки
     train(players, size, in_a_row, agents, episodes)
